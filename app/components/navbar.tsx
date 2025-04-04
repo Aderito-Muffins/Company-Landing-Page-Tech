4b65acbc9336482fd932a0b45f0db4cb5d521352
Adérito Muffins <157916148+Aderito-Muffins@users.noreply.github.com>
2025-04-04 18:16:44 +0200

    v3

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -40,7 +40,7 @@ const Navbar = () => {
                         )}
                     />
                     <span className='hidden text-lg font-semibold md:block'>
-                        MUFFINS Corp
+                        Muffins Corp.
                     </span>
                 </Link>
                 <div className='flex items-center gap-5'>
